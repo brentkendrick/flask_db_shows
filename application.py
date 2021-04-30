1fec3b26547adbef340c714e9974418f2c597b44
brentkendrick <brentkend@gmail.com>
2021-04-30 05:35:16 -0600

    refactored

--- application.py
+++ application.py
@@ -2,16 +2,13 @@
 
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-import json
 
 # To run the app: on the command line, first:  export FLASK_APP=application
 
 # To test SQLAlchemy commands in the CLI, run python and run "from application import Shows"
 
 
-
 app = Flask(__name__)
-
 
 ENV = 'dev'
 
@@ -30,7 +27,6 @@ db = SQLAlchemy(app)
 
 # Name the class after the table name or use dunder to name the table
 class Shows(db.Model):
-    __tablename__='shows'
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.Text, unique=True)
@@ -56,16 +52,18 @@ def search():
     q = request.args.get("q")
     # q = "Officer"
     if q:
+        # SQL direct query TODO
         # shows2 = db.session.execute("""SELECT * FROM shows WHERE title LIKE ?""", {'%' + q + '%'})
-        # sqlitis translation: select([shows]).where(text('title'))
+
+        # search format with % wildcards
         srch = "%{}%".format(q)
-        shows2 = Shows.query.filter(Shows.title.like(srch)).all()
-        # shows2 = db.session.query(Shows).filter(Shows.title.like(srch)).all()
-        shows2 = [z.to_json() for z in shows2]
+        shows = Shows.query.filter(Shows.title.like(srch)).all()
+        # shows = db.session.query(Shows).filter(Shows.title.like(srch)).all()
+        shows = [z.to_json() for z in shows]
 
     else:
-        shows2 = []
+        shows = []
 
         # for testing 
-        # shows2 = [{"id": 365969, "title": "10-8: Officers on Duty"}, {"id": 1706129, "title": "Officer Ichiro"}, {"id": 1876592, "title": "An Officer and a Movie"}, {"id": 3001516, "title": "Ridin' Dirty with Officer Turner"}, {"id": 3457306, "title": "Bonnie Blake: Parole Officer"}, {"id": 5171576, "title": "Officer Hardbody"}, {"id": 6823170, "title": "Officer Bob"}, {"id": 7575588, "title": "NUTV Officer Election Coverage"}, {"id": 8362676, "title": "Officer Hansel, Burbank PD"}, {"id": 10348734, "title": "Police Code Zero: Officer Under Attack"}, {"id": 10380660, "title": "Young Army Officer"}, {"id": 12295806, "title": "Officer Eric Hale"}, {"id": 12409398, "title": "Officer Down the Next 24 Hours"}, {"id": 13016298, "title": "The Officer Tatum"}]
-    return jsonify(shows2)
+        # shows = [{"id": 365969, "title": "10-8: Officers on Duty"}, {"id": 1706129, "title": "Officer Ichiro"}, {"id": 1876592, "title": "An Officer and a Movie"}]
+    return jsonify(shows)
